0731a28786c200e8ddad343337c3cd81f07d7299
Олег Леонов <oleg@snoonu.com>
2024-03-28 23:01:59 +0800

    Fixed reply bugs

--- telegram-bot.py
+++ telegram-bot.py
@@ -60,10 +60,17 @@ def answer_a_question(update):
 
 
 def reply_a_question(update):
-    text_to_reply = update.message.reply_to_message.text + update.message.text.replace(f'@{botname}', ".")
+    if update.message.reply_to_message.text is not None:
+        text_to_reply = update.message.reply_to_message.text + update.message.text.replace(f'@{botname}', ".")
+        reply_to_message_id = update.message.reply_to_message.message_id
+    else:
+        text_to_reply = update.message.text.replace(f'@{botname}', "")
+        reply_to_message_id = update.message.message_id
+
     answer = generate_answer(update.message.from_user.id, text_to_reply)
+
     update.message.reply_text(text=f'@{update.message.from_user.username} {answer}',
-                              reply_to_message_id=update.message.reply_to_message.message_id)
+                              reply_to_message_id=reply_to_message_id)
 
 
 def simple_reply(update):
